326876349683410c528b58218a5904fb65d957c0
Chiu Chun-Hsien <95356121+911218sky@users.noreply.github.com>
2026-01-06 17:31:15 +0800

    Feat: Pre-install FunASR models in Docker image

--- Docker/install_wrapper.sh
+++ Docker/install_wrapper.sh
@@ -15,6 +15,33 @@ mkdir -p GPT_SoVITS/text
 
 bash install.sh --device "CU${CUDA_VERSION//./}" --source HF
 
+# Download FunASR models using ModelScope SDK (complete downloads)
+echo "[INFO] Downloading FunASR models via ModelScope SDK..."
+pip install modelscope -q
+
+python3 -c "
+from modelscope import snapshot_download
+import os
+
+models_dir = 'tools/asr/models'
+os.makedirs(models_dir, exist_ok=True)
+
+models = [
+    'iic/speech_fsmn_vad_zh-cn-16k-common-pytorch',
+    'iic/punc_ct-transformer_zh-cn-common-vocab272727-pytorch', 
+    'iic/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch'
+]
+
+for model_id in models:
+    model_name = model_id.split('/')[-1]
+    local_dir = os.path.join(models_dir, model_name)
+    print(f'[INFO] Downloading {model_name}...')
+    snapshot_download(model_id, local_dir=local_dir)
+    print(f'[INFO] Downloaded {model_name}')
+
+print('[INFO] All FunASR models downloaded successfully!')
+"
+
 pip cache purge
 
 pip show torch
