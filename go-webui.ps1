09fa7a8c57bab2c9be0885b2daa711b7f3f86761
Chiu Chun-Hsien <95356121+911218sky@users.noreply.github.com>
2026-01-08 13:07:54 +0800

    fix: update go-webui.ps1 path to runtime/python

--- go-webui.ps1
+++ go-webui.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "SilentlyContinue"
 chcp 65001
 Set-Location $PSScriptRoot
-$envPath = Join-Path $PSScriptRoot "runtime\env"
+$envPath = Join-Path $PSScriptRoot "runtime\python"
 $env:PATH = "$envPath;$envPath\Scripts;$env:PATH"
 & "$envPath\python.exe" -I "$PSScriptRoot\webui.py" zh_CN
 pause
